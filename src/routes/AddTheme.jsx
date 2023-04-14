459f756a1e9e8e35f15429575fdd47910f9f7c21
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-04-14 11:46:29 -0400

    configurando editor de texto

--- src/routes/AddTheme.jsx
+++ src/routes/AddTheme.jsx
@@ -1,9 +1,13 @@
-
-
+import "./AddTheme.css";
 const AddTheme = () => {
   return (
-    <div>Agregar Tema</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="editor">Editor</div>
+        <div className="preview">Preview</div>
+      </div>
+    </div>
+  );
+};
 
-export default AddTheme
+export default AddTheme;
