99af0b6b77a30cd0f3a1bfaa4483f83054974e85
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-04-23 20:17:24 -0400

    change

--- src/routes/AddArticle.jsx
+++ src/routes/AddArticle.jsx
@@ -1,7 +1,7 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
-// import "./AddArticle.css";
+import "./AddArticle.css";
 import { Link } from "react-router-dom";
 
 const modules = {
