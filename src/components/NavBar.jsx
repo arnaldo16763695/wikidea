a17ef7f428f50d3825b550f1f98f45fb1abd342a
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-05-04 21:14:48 -0400

    modified of nav , implementing side  menu in desktop view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,16 +6,18 @@ function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="navBar">
-      <div className="nav_logo">Colabry</div>
-      <div className={`nav_items ${isOpen && "open"}`}>
-        <Link onClick={() => setIsOpen(!isOpen)} to="/add-article">Crear artículo</Link>{" "}
-        <Link onClick={() => setIsOpen(!isOpen)} to="/list-categories"> Categorías</Link>
-        <Link onClick={() => setIsOpen(!isOpen)} to="/list-articles"> Articulos</Link>
-      </div>
       <div className={`nav_toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
         <span></span>
         <span></span>
         <span></span>
+      </div>
+      <div className="nav_logo">Colabry</div>
+      <Link className="link-add-content" to="/add-article">Crear Contenido</Link>
+      <div className={`nav_items ${isOpen && "open"}`}>
+        <Link onClick={() => setIsOpen(!isOpen)} to="/">Inicio</Link>{" "}
+        <Link onClick={() => setIsOpen(!isOpen)} to="/add-article">Crear artículo</Link>{" "}
+        <Link onClick={() => setIsOpen(!isOpen)} to="/list-categories"> Categorías</Link>
+        <Link onClick={() => setIsOpen(!isOpen)} to="/list-articles"> Articulos</Link>
       </div>
     </header>
   );
