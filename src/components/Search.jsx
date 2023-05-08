cdde509f84d77ba0d93f65973e8f1702a611e834
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-05-08 19:08:47 -0400

    implemented a simple search engine, still needs improvements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,67 @@
+import React, { useState, useEffect } from "react";
+
 import "./css-components/search.css";
+import { Link } from "react-router-dom";
+
 const Search = () => {
+  const [toSearch, setToSearch] = useState([]);
+  const [articlesList, setArticlesList] = useState([]);
+  const [search, setSearch] = useState("");
+  const url = `https://wikideas.up.railway.app/api/v1/wikideas/articles/`;
+  const getArticles = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setArticlesList(data);
+      console.log(toSearch);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getArticles(url);
+  }, []);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    filter(e.target.value);
+  };
+
+  const filter = (item) => {
+    if (!item) {
+      return setToSearch([]);
+    }
+    const result = articlesList.filter((el) => {
+      if (el.title.toString().toLowerCase().includes(item.toLowerCase())) {
+        return el;
+      }
+    });
+    
+    
+      setToSearch(result);
+    
+  };
+
   return (
-      <form className="form-search" role="search">
-        <input className="input-search" type="text"  />
-      </form>
+    <form className="form-search" role="search">
+      <input
+        autoComplete="off"
+        className="input-search"
+        type="text"
+        value={search}
+        onChange={handleChange}
+      />
+      <div className="articles-list">
+        <ul>
+          {toSearch &&
+            toSearch.map((article) => (
+              <li key={article.id}>
+                <Link to={`/article/${article.id}`}>{article.title}</Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </form>
   );
 };
 
