96b4add564275b05ebc2c838dd169dc839b0e7ed
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-05-13 14:06:09 -0400

    pagination implemented in articles

--- src/routes/Articles.jsx
+++ src/routes/Articles.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import './articles.css'
+import "./articles.css";
+import Pagination from "../components/Pagination";
 
 function Articles() {
   const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [byPage, setByPage] = useState(6);
+  const max = Math.ceil(articles.length / byPage);
   const getArticles = async (url) => {
     try {
       let res = await fetch(url),
         articles = await res.json();
-      console.log(res, articles);
+      // console.log(res, articles);
       setArticles(articles);
       if (!res.ok) throw { status: res.status, statusText: res.statusText };
     } catch (error) {
@@ -24,16 +28,21 @@ function Articles() {
 
   return (
     <div className="list-articles">
-      <h2>Articulos</h2>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>
-            <Link style={{ color: "#000" }} to={`/article/${article.id}`}>
-              {article.title}
-            </Link>
-          </li>
+      <h1>Todos los Articulos</h1>
+
+      {articles
+        .slice((page - 1) * byPage, (page - 1) * byPage + byPage)
+        .map((article) => (
+          <Link
+            className="card-article"
+            style={{ color: "#000" }}
+            to={`/article/${article.id}`}
+            key={article.id}
+          >
+            <div>{article.title}</div>
+          </Link>
         ))}
-      </ul>
+      <Pagination page={page} setPage={setPage} max={max} />
     </div>
   );
 }
