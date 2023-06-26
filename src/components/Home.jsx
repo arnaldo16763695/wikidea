9418b64675d6073a91cee9dbca62688d142566fb
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-06-26 15:55:55 -0400

    initial attemp to create carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,11 @@
-
 import "./css-components/home.css";
-import Search from '../components/Search'
-
+import Search from "../components/Search";
+import { Carousel } from "./Carousel";
 
 const Home = () => {
-
-
   return (
     <div className="home">
-   
-
+      <Carousel />
       {/* <ImagesSlider /> */}
     </div>
   );
