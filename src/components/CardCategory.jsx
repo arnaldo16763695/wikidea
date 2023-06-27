0a62e97a57f146dcb340388df3eb252a1810def9
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-06-27 07:03:27 -0400

    added modifies at CardCategory.jsx , .css ans My carousel.css

--- src/components/CardCategory.jsx
+++ src/components/CardCategory.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+
+import { Link } from "react-router-dom";
 import "./css-components/cardCategory.css";
 const CardCategory = (props) => {
   return (
     <div className="card">
       
-      <img className="product--image" src={props.url} alt="product image" />
-      <h2>{props.name}</h2>
+      <Link to={''}><img className="product--image" src={props.url} alt="product image" />
+      <h3 className="title-card">{props.name}</h3></Link>
       
-      <p>{props.description}</p>
+      {/* <p>{props.description}</p> */}
      
     </div>
   );
