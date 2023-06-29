a33ced901de7c63b5402895ed9f33bda7f9e4b56
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-06-29 07:40:52 -0400

    change of imagesCarousel's folder

--- src/helpers/dataCarousel.js
+++ src/helpers/dataCarousel.js
@@ -21,137 +21,48 @@ export const responsive = {
 
 export const urlImages = [
   {
-    url: "../../public/images/carouselImages/alimentacion-y-cocina.jpg",
+    url: "../../public/images/alimentacion-y-cocina.jpg",
     id: 5,
   },
 
   {
-    url: "../../public/images/carouselImages/ciencia-y-tecnologia.jpg",
+    url: "../../public/images/ciencia-y-tecnologia.jpg",
     id: 2,
   },
 
   {
-    url: "../../public/images/carouselImages/cultura-y-entretenimiento.jpg",
+    url: "../../public/images/cultura-y-entretenimiento.jpg",
     id: 1,
   },
 
   {
-    url: "../../public/images/carouselImages/deportes.jpg",
+    url: "../../public/images/deportes.jpg",
     id: 10,
   },
   {
-    url: "../../public/images/carouselImages/educacion-y-desarrollo.jpg",
+    url: "../../public/images/educacion-y-desarrollo.jpg",
     id: 6,
 
   },
   {
-    url: "../../public/images/carouselImages/medio-ambiente.jpg",
+    url: "../../public/images/medio-ambiente.jpg",
     id: 8,
   },
   {
-    url: "../../public/images/carouselImages/negocios-y-finanzas.jpg",
+    url: "../../public/images/negocios-y-finanzas.jpg",
     id: 7,
   },
   {
-    url: "../../public/images/carouselImages/politica.jpg",
+    url: "../../public/images/politica.jpg",
     id: 9,
   },
   {
-    url: "../../public/images/carouselImages/viaje-y-turismo.jpg",
+    url: "../../public/images/viaje-y-turismo.jpg",
     id: 3,
   },
   {
-    url: "../../public/images/carouselImages/salud.jpg",
+    url: "../../public/images/salud.jpg",
     id: 4,
   },
 ];
 
-export const productData = [
-  {
-    id: 1,
-    imageurl:
-      "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Colorful sneakers",
-    price: "$19.99",
-    description: "Some text about the productos..",
-  },
-  {
-    id: 2,
-    imageurl:
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    name: "Sport sneakers",
-    price: "$21.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 3,
-    imageurl:
-      "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "iWatch",
-    price: "$99.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 4,
-    imageurl:
-      "https://images.unsplash.com/photo-1610824352934-c10d87b700cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Water Bottle",
-    price: "$14.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 5,
-    imageurl:
-      "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Vans sneakers",
-    price: "$38.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 6,
-    imageurl:
-      "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Coco Noir",
-    price: "$149.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 7,
-    imageurl:
-      "https://images.unsplash.com/photo-1589782182703-2aaa69037b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Sunglasses",
-    price: "$38.99",
-    description: "Some text about the product..",
-  },
-  {
-    id: 8,
-    imageurl:
-      "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Dove cream",
-    price: "$49.99",
-    description: "Some text about the product..",
-  },
-];
-
-// export const dataCarousel = () => {
-//   const url =
-//     "https://wikideas-api-klaa.onrender.com/api/v1/wikideas/categories/";
-//   const [categories, setCategories] = useState([]);
-//   useEffect(() => {
-//     setLoading(true);
-//     api.get(url).then((res) => {
-//       // console.log(res);
-//       if (!res.err) {
-//         setCategories(res);
-//         setError(null);
-//       } else {
-//         setCategories(null);
-//         setError(res);
-//       }
-
-//       setLoading(false);
-//     });
-//   }, [url]);
-
-//   return categories;
-// };
