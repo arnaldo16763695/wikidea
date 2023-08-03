4c79c568b205a995ddf6ae0398169c009043702a
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-08-03 19:39:09 -0400

    change navBar background when scrollY is major than zero

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./css-components/navBar.css";
 import { Link } from "react-router-dom";
-import Search from "./Search";
 
 function NavBar(props) {
   const [isOpen, setIsOpen] = useState(false);
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Home from "../components/Home";
 import FooterMobile from "../components/FooterMobile";
 import AvailableCategory from "../components/AvailableCategory";
 import "./root.css";
-import { MyCarousel } from "../components/MyCarousel";
 import CarouselSection from "../components/CarouselSection";
 
 const root = () => {
-  const fecha = new Date()
-  console.log(Date.parse('2023-08-02T20:23:00.174Z'))
+  const [classNavBar, setClassNavBar] = useState(false);
+  const detectScroll = () => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setClassNavBar(true);
+      } else {
+        setClassNavBar(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    detectScroll();
+  }, []);
+
   return (
     <div className="container-root">
-    
       <div className="home-video">
         <video autoPlay muted loop id="myVideo">
           <source src="/images/home-video.mp4" type="video/mp4" />
         </video>
         <div className="shadow-home">
           <header className="header">
-            <NavBar backgroundColor={`background-trans`} />
+            <NavBar
+              backgroundColor={`${
+                classNavBar ? "background-dark" : " background-trans"
+              }`}
+            />
             {/* <SubNavBar fontColor={`white-color`}/> */}
           </header>
           <Home />
@@ -28,7 +43,7 @@ const root = () => {
       </div>
       <AvailableCategory />
       <CarouselSection />
-      
+
       <Footer fontColor={"footer-font-dark"} />
       <FooterMobile
         svgLeft={""}
